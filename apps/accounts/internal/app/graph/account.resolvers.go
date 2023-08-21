92d1a678aee3124f7acf9215dd83a1b108588afb
nxtcoder17 <nxtcoder17@gmail.com>
2023-08-21 16:52:14 +0530

    http-cli and dap updates

--- apps/accounts/internal/app/graph/account.resolvers.go
+++ apps/accounts/internal/app/graph/account.resolvers.go
@@ -76,15 +76,3 @@ func (r *Resolver) AccountIn() generated.AccountInResolver { return &accountInRe
 type accountResolver struct{ *Resolver }
 type accountInResolver struct{ *Resolver }
 
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *accountResolver) IsDeleted(ctx context.Context, obj *entities.Account) (*bool, error) {
-	panic(fmt.Errorf("not implemented: IsDeleted - isDeleted"))
-}
-func (r *accountInResolver) IsDeleted(ctx context.Context, obj *entities.Account, data *bool) error {
-	panic(fmt.Errorf("not implemented: IsDeleted - isDeleted"))
-}
